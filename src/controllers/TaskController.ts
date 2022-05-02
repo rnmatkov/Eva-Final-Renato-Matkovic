5d6dcd615f4792984fe3c94506562544267d9e2a
Renato Matkovic <renato.matkovic.moreira@ciisa.cl>
2022-05-02 12:41:00 -0400

    Github actions for CD

---
+++ src/controllers/TaskController.ts
@@ -0,0 +1,67 @@
+import { Request, Response } from "express"
+import { CreateTaskDTO, TaskDTO, UpdateTaskDTO } from "../models/dto/TaskDTO"
+import TaskRepository from "../models/repositories/TaskRepository"
+import { taskCreateSchema, taskUpdateSchema } from "../models/validators/taskSchemas"
+
+export default class TaskController {
+    public readonly getAll = async (_req: Request, res: Response) => {
+        const repository = new TaskRepository(1)
+        const tasks: TaskDTO[] = await repository.findAll()
+        res.json(tasks)
+    }
+
+    public readonly getById = async (req: Request, res: Response) => {
+        const { id } = req.params
+        const repository = new TaskRepository(1)
+        const task = await repository.findById(parseInt(id))
+
+        if (!task) {
+            res.status(404).json({ message: 'Task not found' })
+            return
+        }
+
+        res.json(task)
+    }
+
+    public readonly create = async (req: Request, res: Response) => {
+        const task = req.body as CreateTaskDTO
+
+        try {
+            await taskCreateSchema.validateAsync(task)
+        } catch (error) {
+            res.status(400).json({ message: error.message })
+            return
+        }
+
+        const repository = new TaskRepository(1)
+
+        const newTask = await repository.create(task)
+
+        res.json(newTask)
+    }
+
+    public readonly update = async (req: Request, res: Response) => {
+        const { id } = req.params
+        const task = req.body as UpdateTaskDTO
+
+        try {
+            await taskUpdateSchema.validateAsync(task)
+        } catch (error) {
+            res.status(400).json({ message: error.message })
+            return
+        }
+        const repository = new TaskRepository(1)
+        await repository.update(parseInt(id), task)
+
+        res.sendStatus(204)
+    }
+
+    public readonly delete = async (req: Request, res: Response) => {
+        const { id } = req.params
+
+        const repository = new TaskRepository(1)
+        await repository.delete(parseInt(id))
+
+        res.sendStatus(204)
+    }
+}
